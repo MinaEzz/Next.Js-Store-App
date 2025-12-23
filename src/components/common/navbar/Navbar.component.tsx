d7cce8cc6eed24506731cb8dd3dbcfb532f6591d
Mina Ezzat <minaezzat98@gmail.com>
2025-12-23 14:06:19 +0200

    feat: navbar starter code created

--- src/components/common/navbar/Navbar.component.tsx
+++ src/components/common/navbar/Navbar.component.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import Container from "../container/Container.component";
 
 export default function Navbar() {
-  return <div>Navbar.component</div>;
+  return (
+    <nav className="w-full border-b">
+      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
+        {/** LOGO */}
+        <div>LOGO</div>
+        {/** SEARCHBAR */}
+        <div>SEARCHBAR</div>
+        {/** CART - DARKMODE - LINKS DROPDOWN */}
+        <div className="flex gap-4 items-center">
+          CART - DARKMODE - DROPDOWN
+        </div>
+      </Container>
+    </nav>
+  );
 }
