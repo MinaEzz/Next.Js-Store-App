fbcd98afb19572f674b259a7ea8eea1df94f0487
Mina Ezzat <minaezzat98@gmail.com>
2025-12-23 17:42:39 +0200

    feat: logo component created

--- src/components/common/navbar/Navbar.component.tsx
+++ src/components/common/navbar/Navbar.component.tsx
@@ -1,11 +1,12 @@
 import Container from "../container/Container.component";
+import Logo from "./logo/Logo.component";
 
 export default function Navbar() {
   return (
     <nav className="w-full border-b">
       <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
         {/** LOGO */}
-        <div>LOGO</div>
+        <Logo />
         {/** SEARCHBAR */}
         <div>SEARCHBAR</div>
         {/** CART - DARKMODE - LINKS DROPDOWN */}
