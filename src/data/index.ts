e77aad66ccb9a24fe6e4f608730c1b4db778c1ce
Mina Ezzat <minaezzat98@gmail.com>
2025-12-24 11:14:46 +0200

    feat: creating navigation dropdown component

--- src/data/index.ts
+++ src/data/index.ts
@@ -12,3 +12,30 @@ export const THEME_MODE = [
     value: "system",
   },
 ];
+
+export const NAVIGATION_LINKS = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Products",
+    href: "/products",
+  },
+  {
+    label: "Favorites",
+    href: "/favorites",
+  },
+  {
+    label: "Cart",
+    href: "/cart",
+  },
+  {
+    label: "Orders",
+    href: "/orders",
+  },
+];
