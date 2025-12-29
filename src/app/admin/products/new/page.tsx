9cd4f0d211c1f5029052148687283baf6f4dc0f3
Mina Ezzat <minaezzat98@gmail.com>
2025-12-29 16:46:31 +0200

    feat: admin routes created - admin layout created - admin sidebard component created

---
+++ src/app/admin/products/new/page.tsx
@@ -0,0 +1,6 @@
+import CreateProduct from "@/components/admin/create-product/CreateProduct.page";
+import React from "react";
+
+export default function page() {
+  return <CreateProduct />;
+}
---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,6 @@
+import Products from "@/components/admin/products/Products.page";
+import React from "react";
+
+export default function page() {
+  return <Products />;
+}
